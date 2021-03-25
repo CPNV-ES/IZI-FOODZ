c95cd18c317c4e4322feaf53c4782cd96f196fc9
zaidschouwey98 <zazar981@hotmail.com>
2021-03-25 12:05:20 +0100

    Carousel

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageRoutingModule } from './homepage-routing.module';
 import { HomepageComponent } from './homepage.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 
 
 @NgModule({
   declarations: [
-    HomepageComponent
+    HomepageComponent,
+    CarouselComponent
   ],
   imports: [
     CommonModule,
